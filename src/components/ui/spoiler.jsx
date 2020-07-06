8b035ba320d01dca59f4c3a88da6c676e173eb16
Kulyk Aleksandr <kulyk.aldr@gmail.com>
2020-07-06 23:25:44 +0300

    Add smooth animation to mobile menu & other fix with menu

--- src/components/ui/spoiler.jsx
+++ src/components/ui/spoiler.jsx
@@ -2,13 +2,36 @@ import React, {useState} from "react"
 import styled from "styled-components"
 import Theme from "../../styles/theme"
 import {FaChevronDown} from "react-icons/fa"
-import {SlideDown} from 'react-slidedown'
-import 'react-slidedown/lib/slidedown.css'
+import SlideToggle from "react-slide-toggle"
 
 const SpoilerBox = styled.div`
   background: ${Theme.layout.lightGrey};
   border-left: 2px solid ${Theme.layout.primaryColor};
   margin: 15px 0;
+
+  /*
+  Slide animation styles
+
+  You may need to add vendor prefixes for transform depending on your desired browser support.
+  */
+
+  .slide-enter {
+    transform: translateY(-100%);
+    transition: .7s cubic-bezier(0, 1, 0.5, 1);
+  }
+
+  .slide-enter-active {
+      transform: translateY(0%);
+  }
+
+  .slide-exit {
+    transform: translateY(0%);
+    transition: .7s ease-in-out;
+  }
+
+  .slide-exit-active {
+      transform: translateY(-100%);
+  }
 `
 
 const SpoilerTitle = styled.div`
@@ -29,17 +52,19 @@ const SpoilerTitle = styled.div`
 `
 
 const SpoilerBody = styled.div`
-  display: ${props => props.open ? "block" : "none"};
-  padding: 20px;
+  padding: 0 20px 0 20px;
   background: #fbfbfb;
-  transform: all .5s ease-in-out;
 `
 
 const Spoiler = ({title, children}) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [toggleEvent, setToggleEvent] = useState(0)
 
   // Toggles the spoiler
-  const toggleSpoiler = () => setIsOpen(!isOpen)
+  const toggleSpoiler = () => {
+    setIsOpen(!isOpen)
+    setToggleEvent(Date.now())
+  }
 
   return (
     <SpoilerBox>
@@ -47,10 +72,24 @@ const Spoiler = ({title, children}) => {
         {title}
         <FaChevronDown/>
       </SpoilerTitle>
-      <SpoilerBody open={isOpen}>
-        <SlideDown>
-          {children}
-        </SlideDown>
+
+      <SpoilerBody>
+        <SlideToggle
+          duration={800}
+          toggleEvent={toggleEvent}
+          collapsed={false}
+        >
+          {({setCollapsibleElement, progress}) => (
+            <div
+              ref={setCollapsibleElement}
+              style={{
+                transform: `translateY(${Math.round(20 * (-1 + progress))}px)`
+              }}
+            >
+              {children}
+            </div>
+          )}
+        </SlideToggle>
       </SpoilerBody>
     </SpoilerBox>
   )
